c30f057b568012e3f5dde4eead23539a4c6534b7
lawrence415610 <34848993+lawrence415610@users.noreply.github.com>
2024-10-18 16:28:12 +1100

    :construction: complete three endpoints and requirements

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 const prisma = new PrismaClient();
 
+enum Error {
+  UsernameAlreadyTaken = "UsernameAlreadyTaken",
+  EmailAlreadyInUse = "EmailAlreadyInUse",
+  ValidationError = "ValidationError",
+  ServerError = "ServerError",
+  ClientError = "ClientError",
+  UserNotFound = "UserNotFound",
+}
+
 app.use(express.json());
 app.use(cors());
 
@@ -23,68 +32,151 @@ function generateSimplePassword(length: number = 8): string {
 }
 
 app.post("/users/new", async (req: Request, res: Response): Promise<any> => {
-  const { email, firstName, lastName, userName } = req.body;
-  const password = generateSimplePassword();
-  const newUser = await prisma.user.create({
-    data: {
-      email,
-      firstName,
-      lastName,
-      userName,
-      password,
-    },
-  });
-  return res.status(200).json({
-    success: true,
-    data: Object.assign({}, newUser),
-  });
+  try {
+    const { email, firstName, lastName, userName } = req.body;
+
+    if (!email || !firstName || !lastName || !userName) {
+      return res.status(400).json({
+        success: false,
+        error: Error.ValidationError,
+      });
+    }
+
+    // email validation
+    const hasExistedEmail = await prisma.user.findFirst({
+      where: { email },
+    });
+    if (hasExistedEmail) {
+      return res.status(409).json({
+        error: Error.EmailAlreadyInUse,
+        success: false,
+      });
+    }
+
+    // username validation
+    const hasExistedUserName = await prisma.user.findFirst({
+      where: { userName },
+    });
+    if (hasExistedUserName) {
+      return res.status(409).json({
+        error: Error.UsernameAlreadyTaken,
+        success: false,
+      });
+    }
+
+    const password = generateSimplePassword();
+    const newUser = await prisma.user.create({
+      data: {
+        ...req.body,
+        password,
+      },
+    });
+    const userWithoutPassword = { ...newUser, password: undefined };
+    return res.status(201).json({
+      success: true,
+      data: userWithoutPassword,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: Error.ServerError,
+    });
+  }
 });
 
 // Edit a user
 app.post(
   "/users/edit/:userId",
   async (req: Request, res: Response): Promise<any> => {
-    const id = req.params.userId;
-    const user = await prisma.user.findUnique({
-      where: {
-        id: Number(id),
-      },
-    });
-    if (!user) {
-      res.status(404).json({
+    try {
+      const id = req.params.userId;
+      const user = await prisma.user.findUnique({
+        where: {
+          id: Number(id),
+        },
+      });
+      if (!user) {
+        res.status(404).json({
+          success: false,
+          error: Error.UserNotFound,
+        });
+      }
+      const { email, userName, firstName, lastName } = req.body;
+      if (!email || !firstName || !lastName || !userName) {
+        return res.status(400).json({
+          success: false,
+          error: Error.ValidationError,
+        });
+      }
+      // email validation
+      const hasExistedEmail = await prisma.user.findFirst({
+        where: { email },
+      });
+      if (hasExistedEmail) {
+        return res.status(409).json({
+          error: Error.EmailAlreadyInUse,
+          success: false,
+        });
+      }
+
+      // username validation
+      const hasExistedUserName = await prisma.user.findFirst({
+        where: { userName },
+      });
+      if (hasExistedUserName) {
+        return res.status(409).json({
+          error: Error.UsernameAlreadyTaken,
+          success: false,
+        });
+      }
+
+      const updatedUser = await prisma.user.update({
+        where: {
+          id: Number(id),
+        },
+        data: { ...user!, ...req.body },
+      });
+      res.status(200).json({
+        success: true,
+        data: updatedUser,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        error: Error.ServerError,
         success: false,
       });
     }
-    const updatedUser = await prisma.user.update({
-      where: {
-        id: Number(id),
-      },
-      data: { ...user!, ...req.body },
-    });
-    res.status(200).json({
-      success: true,
-      data: updatedUser,
-    });
   }
 );
 
 // Get a user by email
 app.get("/users", async (req: Request, res: Response): Promise<any> => {
-  const { email } = req.query;
-  const user = await prisma.user.findUnique({
-    where: {
-      email: String(email),
-    },
-  });
-  if (!user)
-    return res.status(404).json({
-      success: false,
+  try {
+    const { email } = req.query;
+    const user = await prisma.user.findUnique({
+      where: {
+        email: String(email),
+      },
     });
-  const { password, ...userWithoutPassword } = user;
-  res.status(200).json({
-    success: true,
-    data: userWithoutPassword,
-  });
+    if (!user)
+      return res.status(404).json({
+        success: false,
+        error: Error.UserNotFound,
+      });
+    const { password, ...userWithoutPassword } = user;
+    res.status(200).json({
+      success: true,
+      data: userWithoutPassword,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      error: Error.ServerError,
+    });
+  }
 });
 
 app.listen(port, () => {
