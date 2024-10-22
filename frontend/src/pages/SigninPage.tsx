84a396e16de965e395b20e965fde07f97f2e177c
lawrence415610 <34848993+lawrence415610@users.noreply.github.com>
2024-10-22 16:20:53 +1100

    :construction: Add a spinner for loading

--- frontend/src/pages/SigninPage.tsx
+++ frontend/src/pages/SigninPage.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { Layout } from "../components/Layout";
 import { toast } from "react-toastify";
 import { RegistrationForm } from "../components/RegistrationForm";
 import { api } from "../apis";
 import { RegistrationInput, ValidationResult } from "../types";
+import { Spinner } from "../components/Spinner";
 
 function validateForm(input: RegistrationInput): ValidationResult {
   const { email, firstName, lastName, userName } = input;
@@ -18,6 +20,8 @@ function validateForm(input: RegistrationInput): ValidationResult {
 }
 
 const SigninPage: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+
   const handleSubmitRegistrationForm = async (input: RegistrationInput) => {
     const validateResult = validateForm(input);
     if (!validateResult.success) {
@@ -25,18 +29,23 @@ const SigninPage: React.FC = () => {
       return;
     } else {
       try {
+        setLoading(true);
         const result = await api.register(input);
         if (result.data.success) {
+          setLoading(false);
           toast.success(`User ${result.data.data.userName} has been created!`);
         }
       } catch (error) {
+        setLoading(false);
         console.log(error);
         toast.error("Error happened during sending request to server.");
       }
     }
   };
+
   return (
     <Layout>
+      {loading ? <Spinner /> : ""}
       <RegistrationForm
         onSubmit={(input: RegistrationInput) =>
           handleSubmitRegistrationForm(input)
